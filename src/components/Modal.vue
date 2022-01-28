21bc184aeaf2a6e36dfb1b7e3acd2666c614f396
Marcelina <marcelina.sitko@interia.pl>
2022-01-28 22:27:43 +0100

    add teleport

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,22 +1,28 @@
 <template>
-   <div class="modal" :class="{ 'is-active': show }">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-         <header class="modal-card-head">
-            <p class="modal-card-title">{{ title }}</p>
-            <button class="delete" aria-label="close" @click="cancel"></button>
-         </header>
-         <section class="modal-card-body">
-            <slot></slot>
-         </section>
-         <footer class="modal-card-foot">
-            <button class="button is-success" @click="save">
-               Save changes
-            </button>
-            <button class="button" @click="cancel">Cancel</button>
-         </footer>
+   <teleport to="body">
+      <div class="modal" :class="{ 'is-active': show }">
+         <div class="modal-background"></div>
+         <div class="modal-card">
+            <header class="modal-card-head">
+               <p class="modal-card-title">{{ title }}</p>
+               <button
+                  class="delete"
+                  aria-label="close"
+                  @click="cancel"
+               ></button>
+            </header>
+            <section class="modal-card-body">
+               <slot></slot>
+            </section>
+            <footer class="modal-card-foot">
+               <button class="button is-success" @click="save">
+                  Save changes
+               </button>
+               <button class="button" @click="cancel">Cancel</button>
+            </footer>
+         </div>
       </div>
-   </div>
+   </teleport>
 </template>
 
 <script>
